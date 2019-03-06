3a63bc8adf1dc6cc66367af26b724531f25c7758
joeyy5588 <b05901082@ntu.edu.tw>
2019-03-06 18:32:44 +0800

    first commit

---
+++ hw1/hw1-1/data_loader/data_loaders.py
@@ -0,0 +1,57 @@
+from torchvision import datasets, transforms
+from base import BaseDataLoader
+from torch.utils.data import Dataset
+import numpy as np
+import pandas as pd
+import torch
+
+
+
+class MnistDataLoader(BaseDataLoader):
+    """
+    MNIST data loading demo using BaseDataLoader
+    """
+    def __init__(self, data_dir, batch_size, shuffle, validation_split, num_workers, training=True):
+        trsfm = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+            ])
+        self.data_dir = data_dir
+        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        super(MnistDataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+class CsvDataset(Dataset):
+
+    def __init__(self, file_path, transform=None):
+        self.data = pd.read_csv(file_path + 'train.csv')
+        self.label_list = np.array(self.data.iloc[:, 0])
+        self.x_list = np.array(self.data.iloc[:, 1:2])
+        self.y_list = np.array(self.data.iloc[:, 2:])
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        # load image as ndarray type (Height * Width * Channels)
+        # be carefull for converting dtype to np.uint8 [Unsigned integer (0 to 255)]
+        # in this example, i don't use ToTensor() method of torchvision.transforms
+        # so you can convert numpy ndarray shape to tensor in PyTorch (H, W, C) --> (C, H, W)
+        x = self.x_list[index]
+        y = self.y_list[index]
+        x = torch.from_numpy(x).float()
+        y = torch.from_numpy(y).float()
+        if self.transform is not None:
+            x = self.transform(x)
+            y = self.transform(y)
+        return x, y
+
+class CsvDataLoader(BaseDataLoader):
+    """
+    MNIST data loading demo using BaseDataLoader
+    """
+    def __init__(self, data_dir, batch_size, shuffle, validation_split, num_workers, training=True):
+        trsfm = None
+        self.data_dir = data_dir
+        self.dataset = CsvDataset(self.data_dir, transform=trsfm)
+        super(CsvDataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
---
+++ hw1/hw1-1/model/model.py
@@ -0,0 +1,45 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from base import BaseModel
+
+
+class MnistModel(BaseModel):
+    def __init__(self, num_classes=10):
+        super(MnistModel, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, num_classes)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+class CsvModel(BaseModel):
+    def __init__(self):
+        super(CsvModel, self).__init__()
+        self.fc1 = nn.Linear(1, 5)
+        self.fc2 = nn.Linear(5, 10)
+        self.fc3 = nn.Linear(10, 10)
+        self.fc4 = nn.Linear(10, 10)
+        self.fc5 = nn.Linear(10, 10)
+        self.fc6 = nn.Linear(10, 10)
+        self.fc7 = nn.Linear(10, 5)
+        self.fc8 = nn.Linear(5, 1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        x = self.fc4(x)
+        x = self.fc5(x)
+        x = self.fc6(x)
+        x = self.fc7(x)
+        x = self.fc8(x)
+        return x
